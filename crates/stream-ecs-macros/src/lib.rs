3a5b32d24fe791c6d3734f3288b59852dd779a52
tuguzT <timurka.tugushev@gmail.com>
2023-06-07 21:37:24 +0300

    Add some structure for macros crate

--- crates/stream-ecs-macros/src/lib.rs
+++ crates/stream-ecs-macros/src/lib.rs
@@ -5,3 +5,20 @@
 #![warn(clippy::all)]
 #![warn(missing_docs)]
 #![forbid(unsafe_code)]
+
+use proc_macro::TokenStream;
+
+mod component;
+mod resource;
+
+/// Derive macro for `stream_ecs::Component` trait.
+#[proc_macro_derive(Component)]
+pub fn component_derive(annotated_item: TokenStream) -> TokenStream {
+    annotated_item
+}
+
+/// Derive macro for `stream_ecs::Resource` trait.
+#[proc_macro_derive(Resource)]
+pub fn resource_derive(annotated_item: TokenStream) -> TokenStream {
+    annotated_item
+}
