96b9587193559a1e7ac0e887dfe095d45f367755
tuguzT <timurka.tugushev@gmail.com>
2023-06-18 12:42:41 +0300

    Add empty `view` module

--- crates/stream-ecs/src/lib.rs
+++ crates/stream-ecs/src/lib.rs
@@ -12,6 +12,7 @@ pub use ::hlist;
 pub mod component;
 pub mod entity;
 pub mod resource;
+pub mod view;
 pub mod world;
 
 mod ref_mut;
---
+++ crates/stream-ecs/src/view/mod.rs
@@ -0,0 +1,3 @@
+//! Provides utilities for views of entities and their data in ECS.
+
+// TODO view API
