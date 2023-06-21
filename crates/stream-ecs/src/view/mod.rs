25e2196da0142bcb22f0991105c4a8a4538f2db9
tuguzT <timurka.tugushev@gmail.com>
2023-06-21 11:58:58 +0300

    Implement `LendingIterator` for view iterator (with `polonius_the_crab` hack)

--- crates/stream-ecs/src/view/mod.rs
+++ crates/stream-ecs/src/view/mod.rs
@@ -2,6 +2,9 @@
 
 // TODO view API
 
+use lending_iterator::prelude::*;
+use polonius_the_crab::prelude::*;
+
 use crate::entity::Entity;
 
 use self::query::Query;
@@ -24,5 +27,41 @@ where
     pub fn get_mut(&mut self, entity: Entity) -> Option<Q::Item<'_>> {
         let Self { fetch } = self;
         Q::fetch(fetch, entity)
+    }
+}
+
+/// Iterator of the view.
+pub struct ViewIter<'a, Q, E>
+where
+    Q: Query,
+    E: Iterator<Item = Entity>,
+{
+    entities: E,
+    fetch: Q::Fetch<'a>,
+}
+
+#[gat]
+impl<'a, Q, E> LendingIterator for ViewIter<'a, Q, E>
+where
+    Q: Query,
+    E: Iterator<Item = Entity>,
+{
+    type Item<'next> = Q::Item<'next>
+    where
+        Self: 'next;
+
+    fn next(&mut self) -> Option<Self::Item<'_>> {
+        let Self { entities, fetch } = self;
+        let mut fetch = fetch;
+        polonius_loop!(|fetch| -> Option<Q::Item<'polonius>> {
+            let Some(entity) = entities.next() else {
+                polonius_return!(None);
+            };
+            let item = Q::fetch(fetch, entity);
+            if let Some(item) = item {
+                polonius_return!(Some(item));
+            }
+        });
+        None
     }
 }
